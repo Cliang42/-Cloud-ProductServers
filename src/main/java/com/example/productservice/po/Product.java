a175ead46d71366dc94f57df2d1d1f82e8b0d56c
cliang42 <cliang0911@163.com>
2024-03-30 15:32:17 +0800

    commit all

---
+++ src/main/java/com/example/productservice/po/Product.java
@@ -0,0 +1,12 @@
+package com.example.productservice.po;
+import lombok.Data;
+
+@Data
+public class Product {
+    private Long id;
+    private String productName;
+    private Double price;
+    private Long stock;
+
+
+}
